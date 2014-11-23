29333b554c530dac808cb3e1eca2b32eee075137
Damien Neveu <damien@redmart.com>
2014-11-24 00:35:14 +0800

    till exercise 4.1

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -13,19 +13,32 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
+  def size2[A](tree : Tree[A]) : Int = fold(tree)(a => 1)((l,r) => 1 + l + r)
+
   def max(tree : Tree[Int]) : Int = tree match {
     case Leaf(v) => v
     case Branch(l, r) => max(l) max max(r)
   }
+
+  def max2(tree : Tree[Int]) : Int = fold(tree)(a => a)((l,r) => l max r)
 
   def depth[A](tree : Tree[A]) : Int = tree match {
     case Leaf(v) => 1
     case Branch(l, r) => 1+depth(l) max 1+depth(r)
   }
 
+  def depth2[A](tree : Tree[A]) : Int = fold(tree)(a => 1)((l,r) => (1+l) max (1+r))
+
   def map[A,B](tree : Tree[A])(f : A => B) : Tree[B] = tree match {
     case Leaf(v) => Leaf(f(v))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def map2[A,B](tree : Tree[A])(f : A => B) : Tree[B] = fold(tree)(a => Leaf(f(a)) : Tree[B])((l, r) => Branch(l, r))
+
+  def fold[A,B](tree : Tree[A])(leafAction : A => B)(branchAction : (B, B) => B) : B = tree match {
+    case Leaf(v) => leafAction(v)
+    case Branch(l, r) => branchAction(fold(l)(leafAction)(branchAction), fold(r)(leafAction)(branchAction))
   }
 
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,27 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(v) => Some(f(v))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(v) => f(v)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = if(this==None){ob}else{this}
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(v) if f(v) => Some(v)
+    case _ => None
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
