f1f4a2b6fe01a7fdcd3d7d7765c8ea03ec86369d
Damien Neveu <damien@redmart.com>
2015-01-06 08:56:16 +0800

    till 5.13

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -54,6 +54,29 @@ trait Stream[+A] {
   def headOption : Option[A] = foldRight(None: Option[A])( (h, t) => Some(h) )
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  def map2[B](f : A => B): Stream[B] = unfold(this) {
+    case Cons(h, t) => Some( (f(h()), t()) )
+    case Empty => None
+  }
+  def take2(n: Int): Stream[A] = unfold((this, n)) {
+    case (Cons(h, t), x) => if(x>0){Some( h(), (t(), x-1))}else{None}
+    case _ => None
+  }
+  def takeWhile3(p: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(h, t) if(p(h())) => Some(h(), t())
+    case _ => None
+  }
+  def zipWith[B,C](strm: Stream[B])(f: (A,B) => C): Stream[C] = unfold((this, strm)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some( (f(h1(), h2()), (t1(), t2())) )
+    case _ => None
+  }
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = unfold((this, s2)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some( (Some(h1()), Some(h2())), (t1(), t2()) )
+    case (Cons(h1, t1), Empty) => Some( (Some(h1()), None : Option[B]), (t1(), Empty) )
+    case (Empty, Cons(h2, t2)) => Some( (None : Option[A], Some(h2()) : Option[B]), (Empty, t2()) )
+    case (Empty, Empty) => None
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -89,5 +112,6 @@ object Stream {
   def from2(n : Int) : Stream[Int] = unfold(n)( s => Some(n, n+1) )
   def constant2[A](a : A) : Stream[A] = unfold(a)( s => Some(a, a) )
   def fibs2 : Stream[Int] = Stream.cons(0, Stream.cons(1, unfold( (0,1) )( s => Some(s._1 + s._2, (s._2, s._1+s._2)) ) ))
+
 
 }
